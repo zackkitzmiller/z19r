3356eee5d14503f896b5ffb6f4508e8d2c37ba6e
zackkitzmiller <zackkitzmiller@gmail.com>
2014-07-11 14:10:42 -0400

    lastfm proxy w/ cache

--- public/index.php
+++ public/index.php
@@ -28,4 +28,23 @@
 		echo json_encode($client->api('repos')->contributors('zackkitzmiller', 'z19r'));
 	});
 
+	$app->get('/lastfm', function() use ($app) {
+		$app->response->headers->set('Content-Type', 'application/json');
+
+		$url = "http://ws.audioscrobbler.com/1.0/user/ZackKitzmiller/recenttracks.rss";
+		$xml = simplexml_load_file($url);
+		$last_played = $xml->channel->item;
+
+		$ret = array(
+			"title" => (string)$last_played->title[0],
+			'url' => (string)$last_played->link
+		);
+
+		$json = json_encode($ret);
+		$app->etag(md5($json));
+		$app->expires('+10 Minutes');
+
+		echo $json;
+	});
+
 	$app->run();
